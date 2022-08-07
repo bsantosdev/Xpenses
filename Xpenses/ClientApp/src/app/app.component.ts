b55aacc3f5cdc79f31882083a8f414cf29468c8d
Bruno Santos <brunosantos.dev@outlook.com>
2022-08-07 18:54:23 +0200

    added envirnoment variables

--- Xpenses/ClientApp/src/app/app.component.ts
+++ Xpenses/ClientApp/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { environment } from '../environments/environment.prod';
 
 @Component({
   selector: 'app-root',
@@ -6,4 +7,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  client_env_name: string = environment.env_name;
 }
